08ef70de7b1141ead19a60a8e71287b1ccffb33a
Ezeka ifeoma vera <ifeomaezeka01@gmail.com>
2025-01-31 03:07:17 -0800

    created a csv of my output
    A csv file of my recently played songs was made with the use of pandas

--- recentlyplayed.py
+++ recentlyplayed.py
@@ -1,4 +1,5 @@
 #import json
+import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -18,14 +19,34 @@ sp = spotipy.Spotify(auth_manager=sp_oauth)
 recent_tracks = sp.current_user_recently_played(limit=50)
 
 # Print the track names, artist of each tracks and played times
-for item in recent_tracks['items']:
+""" for item in recent_tracks['items']:
     track = item['track']
     played_at = item['played_at']
     artist = track['artists'][0] 
     print(f"Track: {track['name']}")
     print(f"Played at: {played_at}")
     print(f"Artist: {artist['name']}")
-    print("-------" * 10)
+    print("-------" * 10)  """
+
+recent_tracks_ls = []
+
+for item in recent_tracks['items']:
+    track_name = item['track']['name']
+    artist_name = item['track']['artists'][0]['name']
+    played_at = item['played_at']
+    
+    recent_tracks_ls.append({
+        'track_name': track_name,
+        'artist_name': artist_name,
+        'played_at': played_at
+    })
+
+df = pd.DataFrame(recent_tracks_ls)
+
+df.to_csv("recent_tracks.csv", index=False)
+
+#Print the first 5 data
+print(df.head())
 
 
 
